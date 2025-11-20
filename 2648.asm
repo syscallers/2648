1021d7555425a295264f74d704ca941bbf633cea
Avery King <gperson@disroot.org>
2025-11-20 14:23:06 -0800

    Add basic program stub
    The program just exits right now, but we'll implement more routines
    next.

---
+++ 2648.asm
@@ -0,0 +1,14 @@
+# CS 2640.02 Final Project
+# Avery King, Lila Tatum, Josh Guzman, and Shuvashree Basnet
+# 11/12/2025-11/20/2025
+#
+# This program is a 2048 game written in MIPS32 assembly. It uses the Bitmap
+# Display and Keyboard and Display tools together to accept user input and
+# display the game board.
+
+.text
+man:
+	# TODO: Put the main game event loop here
+	# (For now, just exit the program)
+	li $v0, 10
+	syscall
